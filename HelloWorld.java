bf2a527ffa2a978cc4790ce63a1411f754d9dd08
Patrick-B1 <patrickbohn1@gmail.com>
2015-10-16 12:14:55 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,14 @@ public class HelloWorld {
     System.out.println("My name is Patrick");
     System.out.println("What is yours?");
     name = in.nextLine();
-    System.out.println("Your name is "+name);
+    if(name.length()>0){
+        if(name=="Patrick"){
+            System.out.println("You have the same name as me!");
+        }else{
+            System.out.println("Your name is "+name);
+        }
+    } else {
+        System.out.println("Is anyone there?");
+    }
   }
 }
