3c4c5bd1ceaf7c8e4eaecd4adcdb7817d75a016e
Patrick-B1 <patrickbohn1@gmail.com>
2015-10-16 11:20:07 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class HelloWorld {
   public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);  
+    String name;
     System.out.println("My name is Patrick");
+    System.out.println("What is yours?");
+    name = in.nextLine();
+    System.out.println("Your name is "+name);
   }
 }
